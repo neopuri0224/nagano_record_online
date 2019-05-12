ea57391c6d6dfca6968f9b9c783669bd67e4c10e
Takafumi-Kondo <t.kily0121@gmail.com>
2019-05-12 21:35:34 +0000

    [Create] Model contact

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_12_211439) do
+ActiveRecord::Schema.define(version: 2019_05_12_212846) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "contact_content"
+    t.text "responce"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
