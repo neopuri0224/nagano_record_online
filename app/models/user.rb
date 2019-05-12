c5723f32c6de9059f4dd97113ec2334d29a6decc
neopuri0224 <neopuri0224@gmil.com>
2019-05-12 09:15:06 +0000

    Update User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+	has_many :orders, dependent: :destroy
+	has_many :user_products, dependent: :destroy
+	has_many :reviews, dependent: :destroy
+	has_many :favorites, dependent: :destroy
+	has_many :contacts, dependent: :destroy
+	has_many :shipments, dependent: :destroy
 end
