9713a04559b6a2e421b38fd85168b8652114cbc3
neopuri0224 <neopuri0224@gmil.com>
2019-05-12 08:02:18 +0000

    new-model-User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_12_204207) do
+ActiveRecord::Schema.define(version: 2019_05_12_204209) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -45,6 +45,23 @@ ActiveRecord::Schema.define(version: 2019_05_12_204207) do
     t.string "ship_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "last_name"
+    t.string "first_name"
+    t.string "ruby_last_name"
+    t.string "ruby_first_name"
+    t.string "postcode"
+    t.string "address"
+    t.string "phone"
+    t.string "email"
+    t.string "password"
+    t.boolean "admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_users_on_deleted_at"
   end
 
 end
