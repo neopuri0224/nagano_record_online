fabc3eb7dfd0e34c4116f7d36f87856a294dc13e
neopuri0224 <neopuri0224@gmil.com>
2019-05-13 07:05:40 +0000

    routing done

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Rails.application.routes.draw do
-  get 'contacts/index'
-  get 'contacts/show'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'labels/index'
-  get 'labels/new'
-  get 'labels/edit'
+  root to: 'products#index'
+  get '/about' => 'static_pages#about'
+  get '/admin' => 'static_pages#admin'
+  resource :shipment, only:[:create]
+  resources :contacts, only:[:index,:create]
+  resources :users, except:[:new,:create] do
+    resources :contacts, only:[:show, :update]
+  end
+  resources :products do
+    resources :recorded_musics, only:[:create,:destroy]
+    resources :reviews, except:[:index,:show]
+    resources :favorites, only:[:create,:destroy]
+  end
+  resources :user_products, except:[:new,:show,:edit]
+  resources :orders, only:[:new,:create,:index]
+  resources :labels, except:[:show]
+  resources :genre, except:[:show]
+  resources :artist, except:[:show]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
